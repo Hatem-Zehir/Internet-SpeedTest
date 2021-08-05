d2f03bf05a84e1b495b4f6c60ce881e7e2ef06a7
Hatem ZEHIR <76598326+Hatem-Zehir@users.noreply.github.com>
2021-08-05 16:26:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sqlite3
 import speedtest
 from datetime import datetime
+import subprocess
+from subprocess import PIPE, Popen
 
-db = sqlite3.connect("SpeedTest.db")
+db = sqlite3.connect('SpeedTest.db')
 db.row_factory = sqlite3.Row
-db.execute("create table if not exists Speed(Date text, Time text, Download real, Upload real, Ping integer)")
+db.execute('create table if not exists Speed(Date text, Time text, Download real, Upload real, Ping integer, Loss real)')
 
 servers = []
 threads = None
@@ -20,13 +22,20 @@ DL = round(results_dict['download']/(1024*1024), 2)
 UP = round(results_dict['upload']/(1024*1024), 2)
 ping = round(results_dict['ping'], 0)
 
+hostname = "8.8.8.8"
+process = subprocess.Popen(['ping','-c','5',hostname],
+stdout=PIPE, stderr=PIPE)
+stdout, stderr = process.communicate()
+packetloss = float([x for x in stdout.decode('utf-8').split('\n') if x.find('packet loss') != -1][0].split('%')[0].split(' ')[-1])
+
 now = datetime.now()
-dtString = now.strftime("%d %B %Y")
-time = now.strftime("%H:%M")
-db.execute("insert into Speed(Date, Time, Download, Upload, Ping) values (? , ? , ?, ?, ?)",(dtString, time, DL, UP, ping))
+dtString = now.strftime('%d %B %Y')
+time = now.strftime('%H:%M')
+db.execute('insert into Speed(Date, Time, Download, Upload, Ping, Loss) values (? , ? , ?, ?, ?, ?)',(dtString, time, DL, UP, ping, packetloss))
 db.commit()
 
 if __name__ == "__main__":
     print('Download: ', DL, 'Mbps')
     print('Upload: ', UP, 'Mbps')
     print('Ping: ', ping, 'ms')
+    print('Average packet loss: ', packetloss,'%')
